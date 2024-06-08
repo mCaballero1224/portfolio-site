9d55459e0e74f8d284ba42f0b48eb73bf601bb21
Michael Caballero <mcaballero.dev@gmail.com>
2024-06-08 02:57:03 -0700

    Connect web server to MongoDB cluster

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config();
 const PORT = process.env.PORT;
 
 /* SETUP */
+const connectDB = require('./server/config/db');
 const path = require('path'); // import path module
 const express = require('express'); // use express library for the web server
 // import handlebars
@@ -14,6 +15,10 @@ const exphbs = require('express-handlebars'); //
 const app = express(); // create instance of the express object to interact with the web server
 app.engine('.hbs', engine({extname: ".hbs"})); // create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs'); // tell express to use the handlebars engine whenever encountering an ".hbs" file
+
+// Connect DB
+connectDB();
+
 
 
 /* Middleware */
