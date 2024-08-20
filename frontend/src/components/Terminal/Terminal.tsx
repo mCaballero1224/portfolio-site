c99f947b8fd8de8511ee9bdd0435a360334aee57
Michael Caballero <mcaballero.dev@gmail.com>
2024-08-20 04:20:46 -0700

    Start work on terminal emulator

---
+++ frontend/src/components/Terminal/Terminal.tsx
@@ -0,0 +1,12 @@
+import './terminal.css';
+import { Link } from 'react-router-dom';
+
+export default function Terminal() {
+  return (
+    <div className="terminal">
+      <p className="placeholder-msg">This terminal is under construction!</p>
+      <p>See what else I've got to work on at the <Link className="terminal-link" to={`/todo`}>Todo</Link> page!</p>
+      <p className="terminal-prompt">[mcaballero@dev ~]$ <input type="text"/></p>
+    </div>
+  );
+}
