f92066723fc7dadeb6ca7514c931f9b86b2db955
Michael Caballero <mcaballero.dev@gmail.com>
2024-08-20 04:20:04 -0700

    Create brand section/home link for navbar

---
+++ frontend/src/components/NaviBrand/NaviBrand.tsx
@@ -0,0 +1,13 @@
+import './navi.css';
+
+export default function NaviBrand() {
+	return (
+		<div className="navi-brand">
+			<img
+				src="https://placehold.co/50x50"
+				className="d-inline-block"
+			/>
+			<p>Michael's Terminal</p>
+		</div>
+	);
+}
