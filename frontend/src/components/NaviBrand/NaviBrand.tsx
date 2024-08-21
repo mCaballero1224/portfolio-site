f89b57074db1eea3db90f9bad61f56042d629597
Michael Caballero <mcaballero.dev@gmail.com>
2024-08-21 04:22:11 -0700

    Add favicon images

--- frontend/src/components/NaviBrand/NaviBrand.tsx
+++ frontend/src/components/NaviBrand/NaviBrand.tsx
@@ -4,7 +4,7 @@ export default function NaviBrand() {
 	return (
 		<div className="navi-brand">
 			<img
-				src="https://placehold.co/50x50"
+				src="/android-chrome-192x192.png"
 				className="d-inline-block"
 			/>
 			<p>Michael's Terminal</p>
