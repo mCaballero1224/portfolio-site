b1f35ddb804363b28fa21b9cecde4f3f74a89786
Michael Caballero <mcaballero.dev@gmail.com>
2024-08-23 05:33:10 -0700

    Add hackerrankd link to footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -7,6 +7,7 @@ export default function Header() {
           <p>
             <a target="_" href="https://www.linkedin.com/in/mcaballero-dev/"><i className="fab fa-linkedin"></i></a>
             <a target="_" href="https://github.com/mCaballero1224/"><i className="fab fa-github"></i></a>
+            <a target="_" href="https://www.hackerrank.com/profile/mcaballero_dev/"><i className="fab fa-hackerrank"></i></a>
           </p>
       </span>
     </footer>
