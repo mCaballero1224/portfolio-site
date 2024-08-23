8497f014a267077dc676a3b7a9a02f7ce0d1adb0
Michael Caballero <mcaballero.dev@gmail.com>
2024-08-23 05:35:41 -0700

    Make footer links create new browser tab

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,9 +5,9 @@ export default function Header() {
       <hr />
       <span id="socials text-center">
           <p>
-            <a target="_" href="https://www.linkedin.com/in/mcaballero-dev/"><i className="fab fa-linkedin"></i></a>
-            <a target="_" href="https://github.com/mCaballero1224/"><i className="fab fa-github"></i></a>
-            <a target="_" href="https://www.hackerrank.com/profile/mcaballero_dev/"><i className="fab fa-hackerrank"></i></a>
+            <a target="_blank" href="https://www.linkedin.com/in/mcaballero-dev/"><i className="fab fa-linkedin"></i></a>
+            <a target="_blank" href="https://github.com/mCaballero1224/"><i className="fab fa-github"></i></a>
+            <a target="_blank" href="https://www.hackerrank.com/profile/mcaballero_dev/"><i className="fab fa-hackerrank"></i></a>
           </p>
       </span>
     </footer>
